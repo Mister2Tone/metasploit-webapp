e2b0547134b350e2d1fe5999e6f18302bf135754
Mister2Tone <pisit_tansri@hotmail.com>
2017-08-27 16:18:30 +0700

    change template engine from jade to ejs

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var workspaces = require('./routes/workspaces')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/workspaces', workspaces);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
