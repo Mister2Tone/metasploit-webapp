b390f1dee87cb9782d16ba8442dc83a8cf91f358
Mister2Tone <pisit_tansri@hotmail.com>
2017-09-18 23:29:20 +0700

    fix #11 show service in each host

--- routes/hosts.js
+++ routes/hosts.js
@@ -4,8 +4,16 @@ const db = require('../db')
 
 const router = new Router()
 
-router.get('/test', async(req,res) =>{
-	res.send('test hosts router')
+router.get('/:id', async(req,res) =>{
+	const hostId = req.params.id 
+	const data = [ hostId ]
+	const text = 'SELECT * FROM services WHERE host_id=($1)'
+	const { rows } = await db.query(text,data)
+	const services = rows
+	res.render('pages/hosts/show', {
+		hostId,
+		services
+	})
 })
 
 module.exports = router
