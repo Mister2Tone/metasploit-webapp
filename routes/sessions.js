a3de2ffe6e5dc287317b345d152615f46afd793e
Mister2Tone <pisit_tansri@hotmail.com>
2017-11-23 05:35:23 +0700

    list post modules and form for running module what u want to run

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,6 +1,7 @@
 const Router = require('express-promise-router')
 const sessionApi = require('../lib/sessions.js')
-// const workspaceApi = require('../lib/workspaces.js')
+const moduleApi = require('../lib/modules.js')
+const workspaceApi = require('../lib/workspaces.js')
 const db = require('../db')
 
 const router = new Router()
@@ -21,23 +22,94 @@ router.get('/', async(req,res) => {
 
 router.get('/:id/post', async(req,res) => {
 	const sessionId = req.params.id
-	console.log("xyz")
+	var postModulesAll = await sessionApi.getCompatibleSessionModule(sessionId)
+		.then( (result) => {
+			return result.modules
+		}).catch( (err) => {
+			console.log(err)
+		})
+
+	var postModules = {}
+
+	for(var i in postModulesAll){
+		postModules[i] = await moduleApi.getModuleInfo('post',postModulesAll[i])
+					.then( (result) => {
+						let buffer = {
+							name : result.name,
+							fullname : result.fullname,
+							rank : result.rank
+						}
+						return buffer
+					})
+	}
+
 	res.render('pages/sessions/post',{
+		sessionId,
+		postModules
+	})
+})
+
+router.get('/:id/*/new', async(req,res) => {
+	const sessionId = req.params.id
+	const postModuleName = req.params[0]
+
+	await moduleApi.getModuleInfo('post',postModuleName)
+		.then( (result) => {
+			postModuleInfo = result
+		}).catch( (err) => {
+			console.log(err)
+		})
+
+	await moduleApi.getModuleOption('post',postModuleName)
+		.then( (result) => {
+			postModuleOption = result
+		}).catch( (err) => {
+			console.log(err)
+		})
+
+	res.render('pages/sessions/show', {
+		postModuleInfo,
+		postModuleOption,
 		sessionId
 	})
 })
 
-router.post('/:id/post/reboot_now', async(req,res) => {
-	const sessionId = req.params.id
-	console.log(sessionId);
-	var env = {}
-	await sessionApi.writeMeterpreterConsole(sessionId,'reboot\n')
-		.then( (result) => {
-			env = result
-		}).catch( (err) => {
-			res.send(err)
+router.post('/*/run_now', async(req,res) => {
+	const postModulePath = req.params[0]
+	const payload = req.body
+
+	var textPayload = ""
+	for(var prop in payload){
+		if(payload[prop])
+			textPayload += "set "+prop+" "+payload[prop]+"\n"
+	}
+	workspaceApi.runExploit(postModulePath,textPayload)
+	res.redirect('/sessions/status')
+
+})
+
+router.get('/status', async(req,res) => {
+	res.render('pages/sessions/status')
+})
+
+router.get('/status/update', async(req,res) => {
+	res.writeHead(200, {
+		'Content-Type' : 'text/event-stream',
+		'Cache-Control' : 'no-cache',
+		'Connection' : 'keep-alive'
+	})
+
+	var ticker = setInterval( () => {
+		workspaceApi.getMsfCommandDisplay().then((result) => {
+			data = result.data.split('\n').join('(newline)')
+			success = /Post module execution completed/.test(data)
+			if(success){
+				clearInterval(ticker)
+			}
+			res.write('data: ' + data + '\n\n')
 		})
-	res.send(env)
+	},5000)
+
 })
 
 
